f04fe47b62b4b5530e04f2b03ca8b31f2fb91351
Upikk <96323919+Upikk@users.noreply.github.com>
2024-08-07 12:16:30 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -14,6 +14,6 @@ AddEventHandler("txAdmin:events:adminAuth", function(data)
     local isAdmin = data.isAdmin
     if not isAdmin then return end
     local playerId = data.netid
-    print("^2TxAdmin: ^3Permission granted to player: " .. GetPlayerName(playerId) .. " (" .. playerId .. ")")
+    print("^3Permission granted to player: " .. GetPlayerName(playerId) .. " (" .. playerId .. ")^0")
     ExecuteCommand("add_principal identifier." .. GetPlayerIdentifier(playerId, 0) .. " group.txFGGroup")
 end)
