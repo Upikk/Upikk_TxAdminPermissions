05fe0c16628c3cdb81d34ba90d74c6ed851b9d18
Upikk <96323919+Upikk@users.noreply.github.com>
2025-05-06 20:33:08 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,10 +1,3 @@
-ExecuteCommand("add_ace group.bypassPermGroup command allow")
-ExecuteCommand("add_principal resource." .. GetCurrentResourceName() .. " group.bypassPermGroup")
-
-for index, permissionValue in pairs(Config.Permissions) do
-    ExecuteCommand("add_ace group.TXFgGroup " .. permissionValue .. " allow")
-end
-
 AddEventHandler("txAdmin:events:adminAuthorized", function(data)
     if not data.isAdmin then return end
     if Config.Debug then print("Authorized Player with TX Admin access.\nID: " .. data.netId) end
