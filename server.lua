3b278cba50aacdd7d690795353ba425b0146550b
Upikk <96323919+Upikk@users.noreply.github.com>
2025-06-03 14:41:16 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,5 +6,5 @@ AddEventHandler("txAdmin:events:adminAuthorized", function(data)
     if not data.isAdmin then return end
     if Config.Debug then print("Authorized Player with TX Admin access.\nID: " .. data.netId) end
     ExecuteCommand("add_principal identifier." .. GetPlayerIdentifier(data.netId, 0) .. " group.TXFgGroup")
-    exports[Config.FiveguardName]:RefreshPlayerPermissions(data.netId)
+    exports[Config.FiveguardResource]:RefreshPlayerPermissions(data.netId)
 end)
